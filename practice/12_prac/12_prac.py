c658d0ed41d2ac6354a8136ce6c8d0232317ebb8
Яна Морозова <144447387+xNagira@users.noreply.github.com>
2024-11-29 00:54:03 +0300

    Work

---
+++ practice/12_prac/12_prac.py
@@ -0,0 +1,46 @@
+from tkinter import*
+from tkinter import messagebox
+import requests
+import json
+
+window = Tk()
+window.title("Собираем информацию о репозитории")
+window.geometry('400x80')
+def nm_rep():
+    rname = name.get()
+    if not rname:
+        messagebox.showerror("Ошибка", "Введите имя репозитория")
+        return
+
+    url = f"https://api.github.com/users/{rname}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        repdata = response.json()
+        datsave = {
+            'company': None,
+            'created_at': repdata.get('created_at'),
+            'email': None,
+            'id': repdata.get('id'),
+            'name': repdata.get('name'),
+            'url': repdata.get('html_url')
+        }
+
+        with open('repos_info.json', 'w') as file:
+            json.dump(datsave, file, indent=4)
+
+        messagebox.showinfo("Успешно", "Информация о репозитории сохранена в файл 'repos_info.json'")
+    else:
+        messagebox.showerror("Ошибка", "Репозиторий не найден")
+
+
+lbl = Label(window, text="Имя репозитория:")
+lbl.pack()
+
+name= Entry(window, width=60)
+name.pack()
+
+btn = Button(window, text="Получить информацию", command=nm_rep)
+btn.pack()
+
+window.mainloop()
