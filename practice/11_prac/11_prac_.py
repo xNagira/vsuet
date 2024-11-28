4bf8a5c86bca7915fd6eedfd345d61d34138de1b
Яна Морозова <144447387+xNagira@users.noreply.github.com>
2024-11-28 22:32:16 +0300

    Work

--- practice/11_prac/11_prac_.py
+++ practice/11_prac/11_prac_.py
@@ -1,6 +1,7 @@
 from tkinter import*
 from tkinter import ttk, messagebox, filedialog
 
+window= None
 tab1 = None
 tab2 = None
 tab3 = None
@@ -147,8 +148,9 @@ def tekst():
             tx.delete(1.0, END)
             tx.insert(END, content)
 
-window = Tk()
-window.title('Морозова Яна Евгеньевна')
-window.geometry('720x400')
-prog()
-window.mainloop()
+if __name__ == "__main__":
+    window = Tk()
+    window.title('Морозова Яна Евгеньевна')
+    window.geometry('720x400')
+    prog()
+    window.mainloop()
